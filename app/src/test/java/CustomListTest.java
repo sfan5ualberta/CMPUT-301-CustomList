1941ed68adf5e2b262aee7608ddfabe8df78b82f
= <=>
2022-03-09 20:59:46 -0700

    actually fixed the tests this time FINAL

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -32,14 +32,14 @@ public class CustomListTest {
         assertFalse(list.hasCity(city));
         City city_2 = new City("Vancouver", "British Columbia");
         assertFalse(list.hasCity(city_2));
-        list.add(city_2);
+        list.addCity(city_2);
         assertTrue(list.hasCity(city_2));
     }
 
     @Test
     void testDelete() {
         City city = new City("Vancouver", "British Columbia");
-        list.add(city);
+        list.addCity(city);
         assertTrue(list.hasCity(city));
         list.delete(city);
         assertFalse(list.hasCity(city));
@@ -53,14 +53,14 @@ public class CustomListTest {
     void testCountCities(){
         assertEquals(0, list.countCities());
         City city = new City("Vancouver", "British Columbia");
-        list.add(city);
+        list.addCity(city);
         assertEquals(1, list.countCities());
         list.delete(city);
         assertEquals(0, list.countCities());
         City city_2 = new City("Calgary", "Alberta");
-        list.add(city_2);
+        list.addCity(city_2);
         City city_3 = new City("Iqaluit", "Nunavut");
-        list.add(city_3);
+        list.addCity(city_3);
         assertEquals(2, list.countCities());
     }
 
